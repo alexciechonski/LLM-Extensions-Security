a7a91e7b16a83f3eb1da5da8a806a6cce997c975
centralc2115 <zceeaci@ucl.ac.uk>
2024-11-11 22:48:29 +0000

    start

---
+++ read_traffic.py
@@ -0,0 +1,46 @@
+from mitmproxy.io import FlowReader
+import socket
+import json
+
+
+file_path = "path_to_.flow_file"
+output_file = "x.json"
+
+# Dictionary to store destinations, associated IPs, and counts
+destinations = {}
+
+with open(file_path, "rb") as f:
+    reader = FlowReader(f)
+    for flow in reader.stream():
+        if hasattr(flow, "request") and flow.request:
+            host = flow.request.host
+            try:
+                ip_address = socket.gethostbyname(host)
+            except socket.gaierror:
+                ip_address = "IP not found"
+
+            if host in destinations:
+                destinations[host]['count'] += 1
+            else:
+                destinations[host] = {
+                    'ip': ip_address,
+                    'count': 1,
+                }
+
+        elif hasattr(flow, "client_conn"):
+            # Using peername instead of address to resolve DeprecationWarning
+            ip_address = flow.client_conn.peername[0]
+
+            if ip_address in destinations:
+                destinations[ip_address]['count'] += 1
+            else:
+                destinations[ip_address] = {
+                    'ip': ip_address,
+                    'count': 1,
+                }
+
+# Write results to a JSON file
+with open(output_file, "w") as output:
+    json.dump(destinations, output, indent=4)
+
+print(f"Results saved to {output_file}")
